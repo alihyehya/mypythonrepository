5948540e3f69fc44b03fdb3f9c57f56bb1a7e0c4
alihyehya <ali.yehya.extra@outlook.com>
2025-08-14 00:30:08 +0300

    Homework 2 (absolute imports)

---
+++ Homework 2/features/feature1.py
@@ -0,0 +1,4 @@
+from utils.math_utils import add_numbers
+
+def feature1_demo():
+    print("Feature 1 result:", add_numbers(10, 20))
---
+++ Homework 2/main.py
@@ -0,0 +1,3 @@
+from features.feature1 import feature1_demo
+
+feature1_demo()
